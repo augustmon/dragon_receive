1a06e169ffc6fa1c1e1601f45677076c6825526c
augustmon <99360842+augustmon@users.noreply.github.com>
2022-04-07 15:11:12 +0200

    Add files via upload

---
+++ receive.js
@@ -0,0 +1,19 @@
+// input
+
+
+let sendColor = "red";
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(100);
+}
+
+
+
+function draw() {
+    // sendColor = msg[1];
+    background(sendColor);
+
+}
+
+
